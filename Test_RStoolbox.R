b88ed5b0c5598d827fe1c2bcb6a06ee7cbef18d2
MohamedHenriques <32837244+MohamedHenriques@users.noreply.github.com>
2017-10-16 15:31:10 +0100

    Add files via upload

---
+++ Test_RStoolbox.R
@@ -0,0 +1,7 @@
+setwd("D:/Work/FCUL/Doutoramento/R/Mapping_coastal_Habitats_Guinea_Bissau")
+graphics.off()
+rm(list=ls())
+
+library(RStoolbox)
+library(devtools)
+install_github("MohamedHenriques/RStoolbox")
